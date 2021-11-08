894a7df35924a7719481910337c9dc205e51c41e
201710757 <neti69@naver.com>
2021-11-08 14:24:21 +0900

    hyper parameter change

--- stable_baseline/DQN/main.py
+++ stable_baseline/DQN/main.py
@@ -5,7 +5,7 @@ from stable_baselines3 import DQN
 env = gym.make("CartPole-v1")
 
 model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=1000000, log_interval=4)
+model.learn(total_timesteps=100000, log_interval=4)
 model.save("dqn_cartpole_v1")
 
 del model # remove to demonstrate saving and loading
